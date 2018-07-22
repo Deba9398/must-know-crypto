b333372f8470f3104dea90476784599baf6466ac
Derek Baumgartner <Derek.Baumgartner@gmail.com>
2018-07-22 12:52:56 -0600

    Move website list to separate file.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ A site with links to various cryptocurrency and blockchain technology websites.
 ## Contributing
 
 If you'd like to add (or remove) as website from the list, simply create a pull request with your changes.
+
+The list of websites is in [website-list.js](https://github.com/Deba9398/must-know-crypto/blob/master/website-list.js).
---
+++ website-list.js
@@ -0,0 +1,44 @@
+const WEBSITE_LIST = [
+    {
+        title: "Coin Lists",
+        links: [
+            { title: 'CoinMarketCap', url: 'https://coinmarketcap.com/' },
+            { title: 'TradingView', url: 'https://www.tradingview.com/markets/cryptocurrencies/prices-all/' },
+            { title: 'CoinCap', url: 'https://coincap.io/' },
+            { title: 'EveningStar', url: 'https://eveningstar.io/' },
+            { title: 'CoinGecko', url: 'https://www.coingecko.com/' },
+            { title: 'CryptoCompare', url: 'https://www.cryptocompare.com/coins/' },
+            { title: 'BitInfoCharts', url: 'https://bitinfocharts.com/top-cryptocurrency-list.html' },
+            { title: 'CryptoCoinCharts', url: 'https://cryptocoincharts.info/coins/info' },
+            { title: 'Yahoo Finance', url: 'https://finance.yahoo.com/cryptocurrencies' },
+            { title: 'FlipHodl', url: 'https://fliphodl.com/token-market/' }
+        ]
+    },
+    {
+        title: 'Exhanges',
+        links: [
+            { title: 'List of Exchanges', url: 'https://coinmarketcap.com/exchanges/volume/24-hour/' },
+            { title: 'Coinbase', url: 'https://www.coinbase.com/' },
+            { title: 'Binance', url: 'https://www.binance.com/' },
+            { title: 'Bittrex', url: 'https://bittrex.com/' },
+            { title: 'Kucoin', url: 'https://www.kucoin.com/' },
+            { title: 'Gemini', url: 'https://gemini.com/' },
+            { title: 'IDEX', url: 'https://idex.market' }
+        ]
+    },
+    {
+        title: 'Crypto News',
+        links: [
+            { title: 'CryptoPanic', url: 'https://cryptopanic.com/' },
+            { title: 'CoinMarketCal', url: 'https://coinmarketcal.com/' },
+            { title: 'CoinTelegraph', url: 'https://cointelegraph.com/' }
+        ]
+    },
+    {
+        title: 'Heatmaps',
+        links: [
+            { title: 'Coin360', url: 'https://coin360.io/' },
+            { title: 'Yahoo Finance', url: 'https://finance.yahoo.com/cryptocurrencies/heatmap' }
+        ]
+    }
+]
